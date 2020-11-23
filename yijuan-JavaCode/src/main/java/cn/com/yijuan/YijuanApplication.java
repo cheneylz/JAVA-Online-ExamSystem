c2405a8bec162f3e1ce2bd93a0ede783914ab56d
cheneylz <513582632@qq.com>
2020-11-23 10:42:06 +0800

    项目创建

---
+++ yijuan-JavaCode/src/main/java/cn/com/yijuan/YijuanApplication.java
@@ -0,0 +1,13 @@
+package cn.com.yijuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YijuanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YijuanApplication.class, args);
+    }
+
+}
---
+++ yijuan-JavaCode/src/test/java/cn/com/yijuan/yijuan/YijuanApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.yijuan.yijuan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YijuanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
